cad337afd82fbc08b23cb41d4d9dfd05030d484b
marciuto <formante.marco@gmail.com>
2023-04-17 19:35:11 +0200

    create components folder & create Header.js file

---
+++ app/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+    return (
+      <>
+    <header className='header'>
+      <div> sadjk</div>
+    </header>
+    </>
+  )
+}
+
+export default Header
