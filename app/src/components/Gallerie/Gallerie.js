d73f44ac5bd5fdcd2eb383429444eec740f7cef8
marciuto <formante.marco@gmail.com>
2023-04-22 02:42:01 +0200

    change loading method to change Loading state

--- app/src/components/Gallerie/Gallerie.js
+++ app/src/components/Gallerie/Gallerie.js
@@ -1,29 +1,32 @@
-import React, { useState ,useEffect,useCallback, Component} from 'react'
+import React, { useState ,useEffect} from 'react'
 import GallerieCard from './GallerieCard'
 import data_gallerie from './data-gallerie'
 
 
 const Gallerie = () => {
-  let [lo,setlo] =useState(true)
+  let [loading, setloading] = useState(true)
+  
+  let indexMap = 0;
  
+
+
   useEffect(() => {
-    if (document.querySelectorAll("figure").length > 35) {
+
+    if (indexMap === document.querySelectorAll("figure").length) {
       setTimeout(() => {
-        setlo(false) 
-        console.log(lo);
+        setloading(false) 
       }, 1000);
     
     }
-
-
-  },[])
+  }, [])
+  
 
   return (
     <div className='gallerie-container'>
-      <div style={lo ? {position:"fixed",zIndex:100,top:0,left:0,width:"100vw",height:"100vh",backgroundColor:"black" , color:"white",display:"flex",alignItems:"center",justifyContent:"center",fontSize:"46px"}:{display:"none"}}>loading</div>
+      <div style={loading ? {position:"fixed",zIndex:100,top:0,left:0,width:"100vw",height:"100vh",backgroundColor:"black" , color:"white",display:"flex",alignItems:"center",justifyContent:"center",fontSize:"46px"}:{display:"none"}}>loading</div>
       {
         data_gallerie.map((data) => {
-         
+          indexMap++;
           return (
              
               <GallerieCard src={data.src}
@@ -32,8 +35,8 @@ const Gallerie = () => {
                 to={data.to + data.caterory}
                 key={data.id}
                 className={
-                  data.id === 1 ? "card__center--top" : data.id === 4 ?
-                    "card__center--bottom" : "gallerie__card"}
+                  data.id === 1 ? "gallerie__card card__center--top" : data.id === 4 ?
+                    "gallerie__card card__center--bottom" : "gallerie__card"}
               />
               )
 
